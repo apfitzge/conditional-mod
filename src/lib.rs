4f4bea7c85d79ea1daf5b7d84a4679693320d3b6
Andrew Fitzgerald <apfitzge@gmail.com>
2025-03-14 09:57:20 -0500

    conditional_pub_mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
-
+/// Conditionally declares a mod as public.
+macro_rules! conditional_pub_mod {
+    ($name:ident, $cond:meta) => {
+        #[cfg($cond)]
+        pub mod $name;
+        #[cfg(not($cond))]
+        mod $name;
+    };
+}

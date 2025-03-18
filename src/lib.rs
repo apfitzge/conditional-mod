8ead792848a283968520403c1be617d4a1d11e60
Andrew Fitzgerald <apfitzge@gmail.com>
2025-03-18 11:43:21 -0500

    no fallback visibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 /// Conditionally declares a mod as public.
 #[macro_export]
 macro_rules! conditional_vis_mod {
+    ($name:ident, $cond:meta, $conditional_vis:vis) => {
+        #[cfg($cond)]
+        $conditional_vis mod $name;
+        #[cfg(not($cond))]
+        mod $name;
+    };
     ($name:ident, $cond:meta, $conditional_vis:vis, $fallback_vis:vis) => {
         #[cfg($cond)]
         $conditional_vis mod $name;

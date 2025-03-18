2fe7417e332510f628a23d5c53b3f8e91b3e3488
Andrew Fitzgerald <apfitzge@gmail.com>
2025-03-18 11:50:48 -0500

    Document arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 /// Conditionally declares a mod as public.
+///
+/// Arguments:
+/// - name - The name of the module being declared
+/// - cond - The condition for `conditional_vis`
+/// - conditional_vis - The visibility to use if `cond` is true
+/// - fallback_vis - Optional visibility to use if `cond` is false
 #[macro_export]
 macro_rules! conditional_vis_mod {
     ($name:ident, $cond:meta, $conditional_vis:vis) => {
